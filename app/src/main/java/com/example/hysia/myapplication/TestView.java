17516ad178553474a1f68449d138874f43821cda
KimMunseop <hysia11@gmail.com>
2017-02-21 16:49:30 +0900

    test branch

---
+++ app/src/main/java/com/example/hysia/myapplication/TestView.java
@@ -0,0 +1,14 @@
+package com.example.hysia.myapplication;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by hysia on 2017. 2. 21..
+ */
+
+public class TestView extends View {
+    public TestView(Context context) {
+        super(context);
+    }
+}
